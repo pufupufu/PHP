3ebb135b629dc4eccb50dc34055088f68582a548
pufupufu <55405796+pufupufu@users.noreply.github.com>
2020-09-10 17:39:10 -0300

    D10M09A20H1739

--- IP/trabajoPractico3/ejercicio10a.php
+++ IP/trabajoPractico3/ejercicio10a.php
@@ -10,37 +10,38 @@ $num = trim(fgets(STDIN));
 
 //ULTIMO DIGITO
 $aux = (($num % 10) % 10) + 7;
-$es10 = $aux >= 10;
-$es10 ? $aux = $aux % 10 : $aux = $aux;
-$encriptadoAux = $encriptadoAux + $aux;
+$es10 = $aux >= 10; //Verifico si el auxiliar es mayor o igual a 10 despues de la suma
+$es10 ? $aux = $aux % 10 : $aux = $aux; //Si es mayor saca el resto de dividir por 10, sino lo deja
+$encriptadoAux += $aux;
 
+//SEGUNDO DIGITO
 $aux = ((int)(($num % 100) / 10) % 10) + 7;
 $es10 = $aux >= 10;
 $es10 ? $aux = $aux % 10 : $aux = $aux;
-$encriptadoAux = $encriptadoAux + ((int)$aux * 10);
+$encriptadoAux += ((int)$aux * 10);
 
+//TERCER DIGITO
 $aux = ((int)(($num % 1000) / 100) % 10) + 7;
 $es10 = $aux >= 10;
 $es10 ? $aux = $aux % 10 : $aux = $aux;
-$encriptadoAux = $encriptadoAux + ((int)$aux * 100);
+$encriptadoAux += ((int)$aux * 100);
 
+//TERCER DIGITO
 $aux = ((int)($num / 1000) % 10) + 7;
-$es10 = $aux >= 10;
+$es10 = $aux >= 10; 
 $es10 ? $aux = $aux % 10 : $aux = $aux;
-$encriptadoAux = $encriptadoAux + (int)($aux * 1000);
-
-echo "ENCRIPTADO AUXILIAR: ". $encriptadoAux. " \n";
+$encriptadoAux += (int)($aux * 1000);
 
 $aux = (int)($encriptadoAux / 1000);
 $aux2 = (int)(($encriptadoAux % 100) / 10);
-$encriptado += (String)($aux * 10);
-$encriptado += (String)($aux2 * 1000);
+$encriptado += ($aux * 10);
+$encriptado += ($aux2 * 1000);
 $aux = $encriptadoAux % 10;
 $aux2 = (int)(($encriptadoAux % 1000) / 100);
 $encriptado += ($aux * 100);
 $encriptado += $aux2;
 
-$menorMil = $encriptado < 1000;
-$menorMil ? $encriptado = "0".$encriptado : $encriptado = $encriptado;
+$menorMil = $encriptado < 1000; //Verifico si al numero le quedo un 0 adelante, por lo que seria menor a 1.000
+$menorMil ? $encriptado = "0".$encriptado : $encriptado = $encriptado; //Si es menor le concatena un 0 al principio
 
 echo $num. " encriptado es: ". $encriptado;
