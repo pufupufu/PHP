f8d7b487d04715a0a5a9d2a677efb3ba5ae73b05
Santiago Scantamburlo <saflemsca@gmail.com>
2021-03-19 15:23:56 -0300

    D19M03A21H1523

--- IPOO/vinotecaNqn.php
+++ IPOO/vinotecaNqn.php
@@ -4,7 +4,10 @@
 //array $coleccionVinos
 //array $datosVinos
 
+$datosVinos = [];
+//Realizo la carga de la coleccion de los vinos
 $coleccionVinos = [
+    //A cada posicion le asigno arreglos donde se almacenan los datos de los vinos
     "Malbec" => [
         0 => ["cantidadBotellas" => 25, "anioProd" => 2001, "precioU" => 251.3],
         1 => ["cantidadBotellas" => 30, "anioProd" => 1999, "precioU" => 230.5],
@@ -20,13 +23,19 @@ $coleccionVinos = [
     ],
 ];
 
+//Muestra por pantalla el contenido de la coleccion
 print_r($coleccionVinos);
-$datosVinos = cantidadYPromedio($coleccionVinos);
+$datosVinos = cantidadYPromedio($coleccionVinos); //Invoco al modulo que va a crear el arreglo con los datos
 
+//Muestra por pantalla la cantidad de botellas y precio promedio de cada tipo de vino
 print_r($datosVinos);
 
 /**
  * MODULO cantidadYPromedio
+ *
+ * Este modulo recibe la coleccion de vinos por parametro y retorna un arreglo con los datos
+ * de la cantidad de botellas de cada tipo de vino y el precio promedio
+ *
  * @param array $colVinos
  * @return array $datos
  */
@@ -37,8 +46,8 @@ function cantidadYPromedio($colVinos)
     //float $precioTotal, $precioPromedio
     $datos = [];
     $i = 0;
-    while ($i < count($colVinos)) {
-        switch ($i) {
+    while ($i < count($colVinos)) { //Itera mientras se encuentre dentro del arreglo
+        switch ($i) { //Segun cada posicion del arreglo, se asigna un diferente tipo de vino
             case 0:
                 $vino = "Malbec";
                 break;
@@ -52,16 +61,20 @@ function cantidadYPromedio($colVinos)
 
         $sumaBotellas = 0;
         $precioTotal = 0;
-        for ($j = 0; $j < count($colVinos[$vino]); $j++) {
+        for ($j = 0; $j < count($colVinos[$vino]); $j++) { //Realiza la suma de las botellas y los precios
             $sumaBotellas += $colVinos[$vino][$j]["cantidadBotellas"];
             $precioTotal += $colVinos[$vino][$j]["precioU"];
         }
+        //Calcula el promedio del precio por botella
         $precioPromedio = $precioTotal / $sumaBotellas;
+        //Asigna ambas variables al arreglo con los datos
         $botellasYPrecio = [
             "totalBotellas" => $sumaBotellas,
             "precioPromedio" => $precioPromedio,
         ];
-        $datos[count($datos)] = $botellasYPrecio;
+        //Asigna el arreglo anterior a la ultima posicion del arreglo que sera retornado
+        $datos[$vino] = $botellasYPrecio;
+        //Pasa a la siguiente posicion
         $i++;
     }
     return $datos;
